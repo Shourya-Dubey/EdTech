8a72263bf42fd68ef017616434241782ae6cc078
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-28 15:21:04 +0530

    verifySignature handler

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -91,3 +91,75 @@ exports.capturePayment = async(req, res) => {
             });
         }
 };
+
+
+//verify signature of Razorpay and server
+exports.verifySignature = async(req, res) => {
+    const webhookSecret = "12345678";
+
+    const signature = req.headers["x-razorpay-signature"];
+
+    const shasum = crypto.createHmac("sha256", webhookSecret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest("hex");
+
+    if(signature === digest){
+        console.log("Payment is Authorised");
+
+        //fetch courseID and userID from notes
+        const {courseId, userId} = req.body.payload.payment.entity.notes;
+
+        try{
+            //find the course and enroll the student in it
+            const enrolledCourse = await Course.findOneAndUpdate(
+                {_id: courseId}, {$push:{studentsEnrolled: userId}},
+                {new: true}
+            );
+
+            if(!enrolledCourse){
+                return res.status(500).json({
+                    success: false,
+                    message: "Course not Found"
+                });
+            }
+
+            console.log(enrolledCourse);
+
+            //find the student and add the course to their list enrolled courses
+            const enrolledStudent = await User.findOneAndUpdate(
+                {_id: userId},
+                {$push:{courses:courseId}},
+                {new:true}
+            );
+
+            console.log(enrolledStudent);
+
+            //mail send of confirmation
+            const emailResponse = await mailSender(
+              enrolledStudent.email,
+              "Congratulations from codeHelp",
+              "Congratulations , you are onboarder into new codehelp"
+            );
+
+            console.log(emailResponse);
+            return res.status(200).json({
+                success: true,
+                message:"Signature Verifies and Course Added"
+            });
+
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: error.message
+            })
+        }
+
+    }else{
+        return res.status(400).json({
+            success: false,
+            message: "Invalid request"
+        })
+    }
+
+};
