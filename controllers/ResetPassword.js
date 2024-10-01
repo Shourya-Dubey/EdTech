823c3bdf27393b536feb7a1284d40dbf81e428aa
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-02 00:32:45 +0530

    Allow All Origins

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -35,6 +35,7 @@ exports.resetPasswordToken = async(req, res) => {
 
       //create url
       const url = `http://localhost:3000/update-password/${token}`;
+      // const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
 
       //send mail containing url
       await mailSender(
